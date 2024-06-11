e1193f5d6fcb5cba883e1691951ba4dfac56bb75
DemoDevv <mathieulebras95@gmail.com>
2024-06-11 10:57:03 +0200

    ajout de méthodes utiles pour les hash

---
+++ services/users/api/helpers/hash.mjs
@@ -0,0 +1,27 @@
+import bcrypt from "bcrypt";
+
+// Number of rounds to use for hashing
+const saltRounds = 10;
+
+/**
+  Hashes a password
+
+  * @param {string} password
+  * @return {string} the hashed password
+*/
+function hashPassword(password) {
+  return bcrypt.hashSync(password, saltRounds);
+}
+
+/**
+  Compares a password with a hash
+
+  * @param {string} password
+  * @param {string} hash
+  * @return {boolean} true if the password matches the hash, false otherwise
+*/
+function comparePassword(password, hash) {
+  return bcrypt.compareSync(password, hash);
+}
+
+export { hashPassword, comparePassword };
