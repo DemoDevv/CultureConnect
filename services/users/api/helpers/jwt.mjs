bf852d8e12a69f6dec33d048ebf477338daaf037
DemoDevv <mathieulebras95@gmail.com>
2024-06-11 10:58:19 +0200

    ajout des helpers pour les jwt

---
+++ services/users/api/helpers/jwt.mjs
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+
+/**
+Generates an access token for the user
+@param {Object} user
+@return {string} the token
+*/
+export function generateAccessToken(user) {
+  return jwt.sign(user, process.env.PRIVATE_KEY, { expiresIn: "15m" });
+}
