ea2b1de6dfbbb75df09a57db6a999e50a4319c7d
MBausson <maximilienbausson@icloud.com>
2024-06-14 11:41:37 +0200

    Added tests

--- services/CultureAPI/tests/models/museum_test.mjs
+++ services/CultureAPI/tests/models/museum_test.mjs
@@ -10,14 +10,48 @@ describe("Museum model test", function () {
       longitude: 20,
       latitude: 20,
     },
+    department: "Loire-Atlantique",
     address: "Road 1",
     url: "https://example.com",
     city: "Nantes",
   });
 
+  const csvData = {
+    "Identifiant muséofile": "M1",
+    "Nom officiel du musée": "Museum1",
+    Longitude: 20,
+    Latitude: 20,
+    Adresse: "Road 1",
+    URL: "https://example.com",
+    Commune: "Nantes",
+  };
+
   it("create", () => {
     expect(museum).have.property("city", "Nantes");
     expect(museum.coordinates).have.property("longitude", 20);
     expect(museum).have.property("name", "Museum1");
+  });
+
+  it("create from csv data", function () {
+    const museumCsv = Museum.fromCsvData(csvData);
+
+    expect(museumCsv).have.property("city", museum.city);
+    expect(museumCsv.coordinates).have.property(
+      "longitude",
+      museum.coordinates.longitude
+    );
+    expect(museumCsv).have.property("name", museum.name);
+  });
+
+  describe("isInIleDeFrance", function () {
+    it("not in ile de france", function () {
+      expect(museum.isInIleDeFrance()).be.false;
+    });
+
+    it("in ile de france", function () {
+      museum.department = "Paris";
+
+      expect(museum.isInIleDeFrance()).be.true;
+    });
   });
 });
