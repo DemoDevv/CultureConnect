265acf10b76b3025cc92c462e3f828f2858b3440
DemoDevv <mathieulebras95@gmail.com>
2024-06-19 13:26:24 +0200

    ajout de documentation de components

--- client/src/components/RequireAuth.jsx
+++ client/src/components/RequireAuth.jsx
@@ -1,6 +1,13 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
 
+/**
+ * Component to require authentication
+ * if the user is not authenticated, it will redirect to the login page
+ * @typedef RequireAuthProps
+ * @param {Object} props
+ * @param {React.ReactNode} props.children
+ */
 export default function RequireAuth({ children }) {
   const { token } = useAuth();
   const { location } = useLocation();
