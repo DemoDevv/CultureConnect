34fe742c22170ea689e059ea6cf1088638f83312
MBausson <maximilienbausson@icloud.com>
2024-06-21 10:36:37 +0200

    renamed service to ratp

--- services/proxy_ratp/api/route.mjs
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const routes = express.Router();
-
-routes.route("/").get(async (req, res) => {
-  res.status(200).send("Hello world !");
-});
-
-export default routes;
--- services/ratp/api/route.mjs
+++ services/ratp/api/route.mjs
@@ -3,7 +3,7 @@ import express from "express";
 const routes = express.Router();
 
 routes.route("/").get(async (req, res) => {
-	res.status(200).send("Hello world !");
+  res.status(200).send("Hello world !");
 });
 
 export default routes;
