c5a3deea385431442ccdec860b349b91fb30d808
DemoDevv <mathieulebras95@gmail.com>
2024-06-21 17:27:02 +0200

    changement des paramètres de la fonction

--- services/ratp/api/helpers/coords.mjs
+++ services/ratp/api/helpers/coords.mjs
@@ -11,18 +11,18 @@ function for calculating the distance between two points on the earth
 @return {number} - distance between the two points in kilometers
 source: https://stackoverflow.com/questions/365826/calculate-distance-between-2-gps-coordinates
 */
-function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
-  var earthRadiusKm = 6371;
+export function distanceInKmBetweenEarthCoordinates(coords1, coords2) {
+  const earthRadiusKm = 6371;
 
-  var dLat = degreesToRadians(lat2 - lat1);
-  var dLon = degreesToRadians(lon2 - lon1);
+  const dLat = degreesToRadians(coords2.lat - coords1.lat);
+  const dLon = degreesToRadians(coords2.lon - coords1.lon);
 
-  lat1 = degreesToRadians(lat1);
-  lat2 = degreesToRadians(lat2);
+  const lat1 = degreesToRadians(coords1.lat);
+  const lat2 = degreesToRadians(coords2.lat);
 
-  var a =
+  const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   return earthRadiusKm * c;
 }
