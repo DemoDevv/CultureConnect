8f1a61b8d092a2b50a1fc50c74d091b749a0724d
DemoDevv <mathieulebras95@gmail.com>
2024-06-24 15:18:12 +0200

    modification des cors de chaque services

--- services/users/app.mjs
+++ services/users/app.mjs
@@ -10,8 +10,8 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-	origin: "http://localhost:8081",
-	optionsSuccessStatus: 200,
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
