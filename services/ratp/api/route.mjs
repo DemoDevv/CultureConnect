1ab35ae7f7e415aab256d59475b0f4be86061cdd
DemoDevv <mathieulebras95@gmail.com>
2024-06-24 12:04:20 +0200

    ajout du cas ou le musée n'éxiste pas

--- services/ratp/api/route.mjs
+++ services/ratp/api/route.mjs
@@ -1,9 +1,43 @@
 import express from "express";
 
+import stopController from "./controllers/stopController.mjs";
+
 const routes = express.Router();
 
 routes.route("/").get(async (req, res) => {
   res.status(200).send("Hello world !");
+});
+
+routes.route("/stops").post(async (req, res) => {
+  /*
+  #swagger.tags = ['Stop']
+  #swagger.description = 'Endpoint to get all stops near to a museum'
+  #swagger.parameters['museum'] = {
+    in: 'body',
+    description: 'Museum object',
+    required: true,
+    type: 'object',
+    schema: {
+      museofile: 'string',
+      name: 'string',
+      coordinates: {
+        longitude: 'number',
+        latitude: 'number',
+      },
+      department: 'string',
+      address: 'string',
+      url: 'string',
+      city: 'string',
+    }
+  }
+  */
+  try {
+    const stops = await stopController.getAllStopsAroundMuseum(req.body);
+    return res.status(200).json(stops);
+  } catch (e) {
+    console.error(e);
+    return res.sendStatus(400);
+  }
 });
 
 export default routes;
