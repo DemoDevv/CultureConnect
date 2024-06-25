23054a395e35eeae6287f75a6c950265a5197945
DemoDevv <mathieulebras95@gmail.com>
2024-06-25 11:03:23 +0200

    wip

--- client/src/routes/home.jsx
+++ client/src/routes/home.jsx
@@ -5,22 +5,23 @@ import List from "../components/List";
 import NavigationBar from "../components/NavigationBar";
 
 export default function Home() {
-	let [results, setResults] = useState([]);
+  let [results, setResults] = useState([]);
+  let [museumSelected, setMuseumSelected] = useState(null);
 
-	return (
-		<>
-			<NavigationBar />
-			<div className="flex flex-col items-center">
-				<Map onChange={setResults} />
-				{results.length == 0 ? (
-					<></>
-				) : (
-					<>
-						<h2>Résultats</h2>
-						<List items={results} />
-					</>
-				)}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <NavigationBar />
+      <div className="flex flex-col items-center">
+        <Map onChange={setResults} />
+        {results.length == 0 ? (
+          <></>
+        ) : (
+          <>
+            <h2>Résultats</h2>
+            <List items={results} />
+          </>
+        )}
+      </div>
+    </>
+  );
 }
