89bf8f4e754f3eaa35afa19602462d6523e96c0b
DemoDevv <mathieulebras95@gmail.com>
2024-06-25 10:49:58 +0200

    ajout de la nouvelle api ratp dans les constants

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,4 +1,5 @@
 export default {
   CULTURE_API_PATH: "http://localhost:8081/api/culture",
   USERS_API_PATH: "http://localhost:8082/api/users",
+  RATP_API_PATH: "http://localhost:8083/api/ratp",
 };
