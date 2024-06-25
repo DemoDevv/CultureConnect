0389d814f1bb4638426fdd81adbcff173a6be0a3
DemoDevv <mathieulebras95@gmail.com>
2024-06-25 10:43:33 +0200

    ajout d'un lien rechercher dans la navbar

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -4,33 +4,34 @@ import { useAuth } from "./AuthProvider";
 import { useRef } from "react";
 
 export default function NavigationBar({}) {
-	const { token } = useAuth();
-	const isAuthenticated = useRef(token !== null);
+  const { token } = useAuth();
+  const isAuthenticated = useRef(token !== null);
 
-	return (
-		<nav className="flex justify-center items-center h-[100px]">
-			<div className="flex justify-between items-center w-3/5 h-1/2 bg-white rounded-[12px] px-5">
-				<Link to={`/`} className="tracking-wide">
-					Culture Connect
-				</Link>
-				<div className="flex justify-between gap-5">
-					{isAuthenticated.current ? (
-						<>
-							{/* add favoris */}
-							<Link to={`/`} className="text-[#B91C1B]">
-								Favoris
-							</Link>
-						</>
-					) : (
-						<>
-							<Link to={`/login`}>Connexion</Link>
-							<Link to={`/register`} className="text-[#B91C1B]">
-								Inscription
-							</Link>
-						</>
-					)}
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="flex justify-center items-center h-[100px]">
+      <div className="flex justify-between items-center w-3/5 h-1/2 bg-white rounded-[12px] px-5">
+        <Link to={`/`} className="tracking-wide">
+          Culture Connect
+        </Link>
+        <div className="flex justify-between gap-5">
+          {isAuthenticated.current ? (
+            <>
+              {/* add favoris */}
+              <Link to={`/`}>Favoris</Link>
+              <Link to={`/home`} className="text-[#B91C1B]">
+                Rechercher
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to={`/login`}>Connexion</Link>
+              <Link to={`/register`} className="text-[#B91C1B]">
+                Inscription
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
