4f5dc9dab2cf72178ac014479833e696e6cdf4db
DemoDevv <mathieulebras95@gmail.com>
2024-06-27 12:04:37 +0200

    documentation de la lib users

--- services/users/api/helpers/jwt.mjs
+++ services/users/api/helpers/jwt.mjs
@@ -6,7 +6,5 @@ Generates an access token for the user
 @return {string} the token
 */
 export function generateAccessToken(user) {
-	//  TODO change back to 15 minutes
-	return jwt.sign(user, process.env.PRIVATE_KEY, { expiresIn: "2h" });
-	// return jwt.sign(user, process.env.PRIVATE_KEY, { expiresIn: "15m" });
+  return jwt.sign(user, process.env.PRIVATE_KEY, { expiresIn: "1h" });
 }
